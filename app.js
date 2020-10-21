1a776bf542f9221d3b9bd3600121b500dbaee95a
Pruthvi <pruthvirajput97@gmail.com>
2020-10-21 23:29:34 +0530

    add mailchimp apis

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-
+const https = require('https');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
@@ -13,14 +13,61 @@ app.get('/',function(req,res){
 
 app.post('/',function(req,res){
 
-    var firstname = req.body.fname;
-    var lastname = req.body.lname;
-    var email = req.body.email;
+    const firstname = req.body.fname;
+    const lastname = req.body.lname;
+    const email = req.body.email;
 
-    console.log(firstname,lastname,email);
+    const data = {
+        members: [
+           { 
+
+               email_address: email,
+               status:'subscribed',
+               merge_fields:{
+
+                FNAME:firstname,
+                LNAME:lastname,
+               }
+                            
+            
+            }
+        
+            
+        ]
+    }
+
+    const jsonData = JSON.stringify(data);
+
+    // const url = https://$API_SERVER.api.mailchimp.com/3.0/lists/audience-api
+    const url = "https://us2.api.mailchimp.com/3.0/lists/a34409d094";
+    const options = {
+        method:'POST',
+        auth:'Pruthvi:6a166384edbdc6a07f7a625feb1ca3c7-us2'
+    }
+
+    const request = https.request(url,options,function(response){
+        response.on('data',function(data){
+
+            console.log(JSON.parse(data));
+
+        })
+
+    })
+
+    request.write(jsonData);
+    request.end();
+
+
     
 });
 
 app.listen(2000,function(){
     console.log('start');
 });
+
+
+// API key
+// 6a166384edbdc6a07f7a625feb1ca3c7-us2
+
+// audience
+// a34409d094
